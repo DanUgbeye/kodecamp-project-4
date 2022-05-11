206cffdbf5a94b0c5e0f5d2e79138a7fbbb05d6e
Daniel Ugbeye <ugbeyellionz@gmail.com>
2022-05-11 12:16:45 +0100

    fixed issue with notfond page

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className=' w-full h-[100vh] text-[#76568A] flex flex-col justify-center items-center '>
+    <div className=' absolute top-[50%] translate-y-[-50%] w-full min-h-[80vh] text-[#76568A] flex flex-col justify-center items-center '>
         <div className=" flex items-center mb-2 ">
             <span className=' text-5xl pr-2 mr-2 font-semibold border-r-2 border-solid border-[#76568A] '>404</span>
             <span className=' text-2xl '>Page Not Found</span>
